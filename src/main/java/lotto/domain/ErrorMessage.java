f87ae2ff11302714b61429f285e6c5170be9148b
LEE-sh1673 <lsh901673@gmail.com>
2022-11-18 18:34:39 +0900

    feat(lotto): 로또 번호에 대한 예외 처리 기능 추가
    - 로또 번호의 개수가 6개가 아닌 경우에 대한 예외 처리 기능 추가
    - 로또 번호에 중복된 숫자가 있는 경우에 대한 예외 처리 기능 추가

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -2,7 +2,9 @@ package lotto.domain;
 
 public enum ErrorMessage {
     LESS_THEN_MINIMUM_AMOUNT("금액은 최소 1,000원입니다."),
-    NOT_DIVISIBLE_WITH_THOUSAND("금액은 1,000원으로 나누어 떨어지는 수여야 합니다.");
+    NOT_DIVISIBLE_WITH_THOUSAND("금액은 1,000원으로 나누어 떨어지는 수여야 합니다."),
+    INVALID_LOTTO_NUMBERS_SIZE("로또번호는 6개를 입력해야 합니다."),
+    DUPLICATED_LOTTO_NUMBER("로또번호는 중복될 수 없습니다.");
 
     private static final String MESSAGE_FORMAT
         = "[ERROR] %s";
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
+import static lotto.domain.ErrorMessage.INVALID_LOTTO_NUMBERS_SIZE;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int MAX_LOTTO_NUMBERS_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +18,25 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        validateSize(numbers);
+        validateDuplicate(numbers);
     }
 
     // TODO: 추가 기능 구현
+    private void validateSize(final List<Integer> numbers) {
+        if (numbers.size() != MAX_LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(
+                INVALID_LOTTO_NUMBERS_SIZE.getMessage()
+            );
+        }
+    }
+
+    private void validateDuplicate(final List<Integer> numbers) {
+        Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+        if (nonDuplicatedNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(
+                ErrorMessage.DUPLICATED_LOTTO_NUMBER.getMessage()
+            );
+        }
+    }
 }
