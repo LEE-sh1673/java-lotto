42fdbfe7d3b8fbfa9436fef25562e1e063a31782
LEE-sh1673 <lsh901673@gmail.com>
2022-11-18 18:15:01 +0900

    feat(lotto): 1000원으로 나누어 떨어지지 않는 경우 대한 예외 처리 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -12,10 +12,31 @@ public class Money {
     }
 
     private void validateAmount(final int amount) {
-        if (amount < MIN_AMOUNT) {
+        validateIsLessThanMinimum(amount);
+        validateUnit(amount);
+    }
+
+    private void validateIsLessThanMinimum(final int amount) {
+        if (isLessThanMinimum(amount)) {
             throw new IllegalArgumentException(
                 "[ERROR] 금액은 최소 1,000원입니다."
             );
         }
+    }
+
+    private boolean isLessThanMinimum(final int amount) {
+        return amount < MIN_AMOUNT;
+    }
+
+    private void validateUnit(final int amount) {
+        if (!isDivisibleWithThousand(amount)) {
+            throw new IllegalArgumentException(
+                "[ERROR] 금액은 1,000원으로 나누어 떨어지는 수여야 합니다."
+            );
+        }
+    }
+
+    private boolean isDivisibleWithThousand(final int amount) {
+        return amount != 0 && amount % MIN_AMOUNT == 0;
     }
 }
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -11,9 +11,19 @@ public class MoneyTest {
     @DisplayName("천원 미만의 금액에 대한 예외 처리")
     @ParameterizedTest
     @ValueSource(ints = {999, 10, 0})
-    void shouldThrowException_underPrice(Integer amount) {
+    void shouldThrowException_underPrice(int amount) {
         assertThatThrownBy(
             () -> new Money(amount)
         ).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("1,000원으로 나누어 떨어지지 않는 경우 대한 예외 처리")
+    @ParameterizedTest
+    @ValueSource(ints = {1_001, 2_100, -1_001})
+    void shouldThrowException_NotDivisibleWithThousand(int amount) {
+        assertThatThrownBy(
+            () -> new Money(amount)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
