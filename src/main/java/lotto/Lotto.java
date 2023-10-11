aba98b1a7ac7f30f585f06d69fef45cc0092c9fb
LEE-sh1673 <lsh901673@gmail.com>
2023-10-11 12:33:42 +0900

    feat: 로또 번호 예외 처리 기능 추가
    - 중복된 숫자 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,37 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private static final int MAXIMUM_LOTTO_SIZE = 6;
+
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateDuplicated(numbers);
+    }
+
+    private void validateDuplicated(final List<Integer> numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (numbers.size() != uniqueNumbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (numbers.size() != MAXIMUM_LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }
