65434c33956edde3955129aaf41c92e0618996b8
LEE-sh1673 <lsh901673@gmail.com>
2023-10-11 12:26:04 +0900

    docs: 기능 목록 수정
    - 로또 번호에 대한 예외 기능 수정

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LottoNumberTest {
 
