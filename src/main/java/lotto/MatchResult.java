a103e095147ee35d6ded2696f276f54e118245de
LEE-sh1673 <lsh901673@gmail.com>
2023-10-12 21:19:15 +0900

    refactor: 열거형 구조 수정
    - 당첨 금액에 대한 열거형 구조 수정

--- src/main/java/lotto/MatchResult.java
+++ src/main/java/lotto/MatchResult.java
@@ -7,12 +7,12 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toMap;
 
 public enum MatchResult {
-    ALL(6, false),
-    FIVE_WITH_BONUS(5, true),
-    FIVE(5, false),
-    FOUR(4, false),
-    THREE(3, false),
-    NONE(0, false);
+    ALL(6, false, LottoRank.FIRST),
+    FIVE_WITH_BONUS(5, true, LottoRank.SECOND),
+    FIVE(5, false, LottoRank.THIRD),
+    FOUR(4, false, LottoRank.FOURTH),
+    THREE(3, false, LottoRank.FIFTH),
+    NONE(0, false, LottoRank.NONE);
 
     private static final Map<Integer, MatchResult> matchesToResult = Stream.of(values())
         .filter((matchResult) -> !matchResult.isBonusMatches())
@@ -22,9 +22,16 @@ public enum MatchResult {
 
     private final boolean bonusMatches;
 
-    MatchResult(int numberOfMatches, boolean bonusMatches) {
+    private final LottoRank rank;
+
+    MatchResult(
+        final int numberOfMatches,
+        final boolean bonusMatches,
+        final LottoRank rank
+    ) {
         this.numberOfMatches = numberOfMatches;
         this.bonusMatches = bonusMatches;
+        this.rank = rank;
     }
 
     public static MatchResult of(
@@ -46,5 +53,9 @@ public enum MatchResult {
 
     public boolean isBonusMatches() {
         return bonusMatches;
+    }
+
+    public int getWinningAmount() {
+        return rank.getWinningAmount();
     }
 }
