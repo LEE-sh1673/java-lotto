1169798541e65f3faacccc5d35ed3792dc61ac79
LEE-sh1673 <lsh901673@gmail.com>
2023-10-17 17:33:18 +0900

    refactor: VO 클래스에 로직 제거
    수익률 계산 로직 이동
    
    as-is:
    - MatchingResult: 일치 내역만 매핑
    - WinningStatistics: 수익률 계산 및 매핑
    
    to-be:
    - MatchingResult: 일치 내역 매핑 + 수익률 계산 및 매핑
    - WinningStatistics: -

--- src/main/java/lotto/model/MatchingResult.java
+++ src/main/java/lotto/model/MatchingResult.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
+import static java.math.RoundingMode.HALF_EVEN;
 import static java.util.stream.Collectors.toList;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumMap;
@@ -14,12 +16,18 @@ public class MatchingResult {
 
     private final EnumMap<MatchingType, Long> countByType;
 
+    private static final BigDecimal REWARD_RATE_FACTOR = new BigDecimal(100);
+
+    public static final int REWARD_RATE_INIT_SCALE = 1;
+
     public MatchingResult(final EnumMap<MatchingType, Long> countByType) {
         this.countByType = countByType;
     }
 
     public WinningStatistics map(final LottoMoney lottoMoney) {
-        return new WinningStatistics(mapPlayResults(), lottoMoney);
+        final List<PlayResult> playResults = mapPlayResults();
+        final BigDecimal profitRate = mapProfitRate(playResults, lottoMoney);
+        return new WinningStatistics(playResults, profitRate);
     }
 
     private List<PlayResult> mapPlayResults() {
@@ -37,5 +45,27 @@ public class MatchingResult {
             matchingType.isBonusMatches(),
             countByType.get(matchingType)
         );
+    }
+
+    private BigDecimal mapProfitRate(
+        final List<PlayResult> playResults,
+        final LottoMoney money
+    ) {
+        if (money.equalsZero()) {
+            return BigDecimal.ZERO;
+        }
+        return calculateTotalAmount(playResults)
+            .multiply(REWARD_RATE_FACTOR)
+            .divide(money.getAmount(), REWARD_RATE_INIT_SCALE, HALF_EVEN);
+    }
+
+    private BigDecimal calculateTotalAmount(final List<PlayResult> playResults) {
+        return playResults.stream()
+            .map(this::calculateAmount)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal calculateAmount(final PlayResult playResult) {
+        return BigDecimal.valueOf(playResult.getCount() * playResult.getWinningAmount());
     }
 }
--- src/main/java/lotto/model/result/WinningStatistics.java
+++ src/main/java/lotto/model/result/WinningStatistics.java
@@ -1,12 +1,8 @@
 package lotto.model.result;
 
-import static java.math.RoundingMode.HALF_EVEN;
-
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
-
-import lotto.model.LottoMoney;
 
 public class WinningStatistics {
 
@@ -14,35 +10,12 @@ public class WinningStatistics {
 
     private final BigDecimal profitRate;
 
-    private static final BigDecimal REWARD_RATE_FACTOR = new BigDecimal(100);
-
-    public static final int REWARD_RATE_INIT_SCALE = 1;
-
     public WinningStatistics(
         final List<PlayResult> playResults,
-        final LottoMoney lottoMoney
+        final BigDecimal profitRate
     ) {
         this.playResults = playResults;
-        this.profitRate = mapProfitRate(lottoMoney);
-    }
-
-    private BigDecimal mapProfitRate(final LottoMoney money) {
-        if (money.equalsZero()) {
-            return BigDecimal.ZERO;
-        }
-        return calculateTotalAmount()
-            .multiply(REWARD_RATE_FACTOR)
-            .divide(money.getAmount(), REWARD_RATE_INIT_SCALE, HALF_EVEN);
-    }
-
-    private BigDecimal calculateTotalAmount() {
-        return playResults.stream()
-            .map(this::calculateAmount)
-            .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    private BigDecimal calculateAmount(final PlayResult playResult) {
-        return BigDecimal.valueOf(playResult.getCount() * playResult.getWinningAmount());
+        this.profitRate = profitRate;
     }
 
     public List<PlayResult> getResults() {
